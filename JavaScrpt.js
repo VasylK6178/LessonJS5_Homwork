4269e1ff738c0389280bee868262d381bef1c7ed
VasylK6178 <ksvsl6178@ukr.net>
2021-07-27 01:06:03 +0300

    processing

---
+++ JavaScrpt.js
@@ -0,0 +1,55 @@
+// - Напишіть код,  який за допомоги document.getElementById або
+// document.getElementsByClassName або document.getElementsByTagName :
+// -- отримує текст з параграфа з id "content"
+
+let getEbyID = document.getElementById('content')
+console.log(getEbyID)
+
+
+// -- отримує текст з блоку з id "rules"
+
+let getID = document.getElementById('rules')
+console.log(getID)
+
+
+// -- замініть текст параграфа з id 'content' на будь-який інший
+
+getEbyID.innerText = 'Массивы наследуют по умолчанию метод valueOf(), который возвращает сам объект, а не простое значение, поэтому при преобразовании массива в число интерпретатор опирается на метод toString(). Пустые массивы преобразуются в пустую строку.'
+
+// -- замініть текст параграфа з id 'rules' на будь-який інший
+
+getID.innerText = 'Если выполняется сравнение объекта с простым значением, оператор выполнит преобразование объекта с использованием правил преобразования в простое значение.'
+
+
+// -- змініть кожному елементу колір фону на червоний
+
+//const getP = document.getElementsByClassName('content');
+getEbyID.style.backgroundColor = 'red';
+getID.style.backgroundColor = 'red';
+
+const getclass = document.getElementsByClassName('fc_rules');
+  for (let ulli of getclass) {
+    ulli.style.backgroundColor = 'red';
+}
+
+// -- змініть кожному елементу колір тексту на синій
+getEbyID.style.backgroundColor = 'blue';
+getID.style.backgroundColor = 'blue';
+for (let ulli of getclass) {
+    ulli.style.backgroundColor = 'blue';
+}
+
+
+// -- отримати весь список класів елемента з id=rules і вивести їх в console.log
+console.log(getID.classList)
+
+
+// -- отримати всі елементи з класом fc_rules. визначити їм 2 події "клік",
+// одна вивдоить текст елементу, інша довільний текст
+
+
+
+// -- поміняти колір тексту у всіх елементів fc_rules на червоний
+for (let ulli of getclass) {
+    ulli.style.backgroundColor = 'red';
+}
